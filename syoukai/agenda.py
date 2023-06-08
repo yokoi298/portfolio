901526f0f664b5e60071b246bb13107b4f96a635
横井陸哉 <92255410+yokoi298@users.noreply.github.com>
2023-06-09 02:22:59 +0900

    Update agenda.py

--- syoukai/agenda.py
+++ syoukai/agenda.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title('ポートフォリオ')
-st.caption('私について少しでも詳しく知ってもら得たら幸いです．')
+st.caption('私について少しでも詳しく知ってもら得たら幸いです．\n\n左のメニューバーからページを遷移できます．')
 
 st.subheader('目次')
 st.write('- page1：自己紹介')
